bfd20b6126b9f18b657509b25aad575afb994f28
l0th3r <alfabetatango9@gmail.com>
2020-10-12 00:46:46 +0200

    need finish backtracking

--- sudokusolver_06-10-2020/sudoku.c
+++ sudokusolver_06-10-2020/sudoku.c
@@ -4,9 +4,16 @@
 #include "useful.h"
 
 int get_grid(int target[9][9], int ac, char **av);
+int gridcmp(int tocmp[9][9], int i, int j);
+int is_modifiable(int grid[9][9], int i, int j);
+int check_all(int grid[9][9], int i, int j);
+int check_row(int grid[9][9], int i, int j);
+int check_col(int grid[9][9], int i, int j);
 
+void arrcpy(int arr1[9][9], int arr2[9][9]);
 void print_grid(int target[9][9], int is_loading);
-void resolve(int grid[9][9]);
+void resolve(int old[9][9]);
+void advance(int *i);
 
 int main(int ac, char **av)
 {
@@ -15,15 +22,148 @@ int main(int ac, char **av)
 
 	grid_res = get_grid(grid, ac, av);
 
-	if (grid_res == 0)
+	if(grid_res == 0)
 		resolve(grid);
 
 	return 0;
 }
 
-void resolve(int grid[9][9])
+void resolve(int old[9][9])
 {
+	/* loops vriables */
+	int i = 0;
+	int j = 0;
+	int k = 1;
+
+	/* conditions variables */
+	int placable = 0;
+
+	/* new grid */
+	int grid[9][9];
+
+	/* copy array */
+	arrcpy(old, grid);
+
+	while(i < 9)
+	{
+		j = 0;
+		while(j < 9)
+		{
+			if (is_modifiable(old, i, j) == 1)
+			{
+				placable = 0;	
+				while(placable == 0 && grid[i][j] < 10)
+				{
+					grid[i][j] += 1;
+					print_grid(grid, 1);
+					placable = check_all(grid, i, j);
+				}
+				if (grid[i][j] == 10)
+				{
+					grid[i][j] = 0;
+				
+				}
+				else if(placable == 1)
+				{
+					/* advance */
+				}
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int check_all(int grid[9][9], int i, int j)
+{
+	int to_return = 0;
+	int row = check_row(grid, i, j);
+	int col = check_col(grid, i, j);
 	
+	if(row == 1 && col == 1)
+		to_return = 1;
+
+	return to_return;
+}
+
+int check_row(int grid[9][9], int i, int j)
+{
+	int to_return = 1;
+
+	/* loops variables */
+	int k = 0;
+
+	while(k < 9) 
+	{
+		if (grid[i][k] == grid[i][j] && i != k)
+			to_return = 0;
+		k++;
+	}
+	return to_return;
+}
+
+int check_col(int grid[9][9], int i, int j)
+{
+	int to_return = 1;
+
+	/* loops variables */
+	int k = 0;
+
+	while(k < 9)
+	{
+		if (grid[k][j] == grid[i][j])
+			to_return = 0;
+		k++;
+	}
+	return to_return;
+}
+
+void advance(int *i)
+{
+	if(*i != 8)
+		*i += + 1;
+	else
+		*i = 0;
+}
+
+int is_modifiable(int grid[9][9], int i, int j)
+{
+	int to_return = 0;
+	
+	if(grid[i][j] == 0)
+		to_return = 1;
+
+	return to_return;
+}
+
+
+int gridcmp(int tocmp[9][9], int i, int j)
+{
+	int to_return = 0;
+
+	if(tocmp[i][j] != 0)
+		to_return = 1;
+
+	return to_return;
+}
+
+
+void arrcpy(int arr1[9][9], int arr2[9][9])
+{
+	/* loops variables */
+	int i = 0;
+	int j = 0;
+
+	while(i < 9)
+	{
+		j = 0;
+		while(j < 9)
+		{
+			arr2[i][j] = arr1[i][j];
+			j++;
+		}
+		i++;
+	}
 }
 
 void print_grid(int target[9][9], int is_loading)
@@ -33,7 +173,7 @@ void print_grid(int target[9][9], int is_loading)
 	int j = 0;
 
 	printf("\033[H\033[J\n");
-	while (i < 9)
+	while(i < 9)
 	{
 		j = 0;
 
@@ -59,7 +199,7 @@ void print_grid(int target[9][9], int is_loading)
 	}
 	printf("\n");
 
-	if (is_loading == 1)
+	if(is_loading == 1)
 		printf("Tring to solve...\n\n");
 }
 
@@ -73,16 +213,13 @@ int get_grid(int target[9][9], int ac, char **av)
 	int j = 0;
 	int cnt = 0;
 
-	/* grid (memory) */
-	int test[9][9];
-
 	/* temporary char */
 	char temp;
 
 	/* grid (file) */
 	FILE *fp;
 
-	if (ac < 2 || strcmp(av[1], "-h") == 0)
+	if(ac < 2 || strcmp(av[1], "-h") == 0)
 	{
 		printf("\nUsage: %s [-h] input_file outpout_file\n\n", av[0]);
 		status = 1;
@@ -104,7 +241,7 @@ int get_grid(int target[9][9], int ac, char **av)
 				while(j < 9)
 				{
 					temp = fgetc(fp);
-					if (temp != '\n' && temp != '\r')
+					if(temp != '\n' && temp != '\r')
 					{
 						target[i][j] = uf_ctoi(temp);
 						cnt++;
