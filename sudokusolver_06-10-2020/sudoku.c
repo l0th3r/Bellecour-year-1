1076a20560e832414945e8603e76dd0e82da70ff
l0th3r <alfabetatango9@gmail.com>
2020-10-13 03:58:49 +0200

    all good need a little check tho

--- sudokusolver_06-10-2020/sudoku.c
+++ sudokusolver_06-10-2020/sudoku.c
@@ -2,36 +2,68 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "useful.h"
-
+int uf_atoi(char *str);
+int uf_ctoi(char c);
 int get_arg(int target[9][9], int ac, char **av);
+int get_out(int target[9][9], char **av);
 int gridcmp(int tocmp[9][9], int i, int j);
 int is_modifiable(int grid[9][9], int i, int j);
 int check_all(int grid[9][9], int i, int j);
 int check_row(int grid[9][9], int i, int j);
 int check_col(int grid[9][9], int i, int j);
 int check_square(int grid[9][9], int i, int j);
+int check_grid(int target[9][9]);
+int check_in_out(int in[9][9], int out[9][9]);
 
 void arrcpy(int arr1[9][9], int arr2[9][9]);
 void print_grid(int target[9][9], int is_loading);
-void resolve(int old[9][9]);
+void resolve(int old[9][9], int grid[9][9]);
 void advance(int *i, int *j);
 void backtrack(int grid[9][9], int old[9][9], int *i, int *j);
+void print_help(char *str);
+void print_usage(char *str);
+void print_no_solve();
+void uf_split_atoi(int *target, char *str, int len);
+void print_loading();
+
+/* grid (file) */
+FILE *fp;
+FILE *out;
+char *out_name;
+
+/* flags variables */
+int f_slow = 1;
+int f_debug = 0;
+int f_very = 0;
+
+/* used for the loading animation */
+int count = 0;
 
 int main(int ac, char **av)
 {
-	int grid[9][9];
-	int arg_res = 0;
+	int input[9][9];
+	int output[9][9];
 
-	arg_res = get_arg(grid, ac, av);
+	int arg_res = 0;
+	int out_res = 0;
+
+	arg_res = get_arg(input, ac, av);
 
 	if(arg_res == 0)
-		resolve(grid);
+	{
+		resolve(input, output);
+
+		/* extract the result */
+		out_res = get_out(output, av);
+
+		/* print one last time to show the result */
+		print_grid(output, 0);
+	}
 
 	return 0;
 }
 
-void resolve(int old[9][9])
+void resolve(int old[9][9], int grid[9][9])
 {
 	/* loops vriables */
 	int i = 0;
@@ -40,38 +72,59 @@ void resolve(int old[9][9])
 
 	/* conditions variables */
 	int placable = 0;
-
-	/* new grid */
-	int grid[9][9];
+	int stop = 0;
 
 	/* copy array */
 	arrcpy(old, grid);
 
+	print_grid(grid, 1);
+	
 	while(i < 9)
 	{
-		if (is_modifiable(old, i, j) == 1)
+		if (is_modifiable(old, i, j) == 1 && stop == 0)
 		{
 			placable = 0;
 			while(placable == 0 && grid[i][j] <= 10)
 			{
 				grid[i][j] += 1;
-				/*printf("tab[%d][%d] = %d\n", i, j, grid[i][j]);*/
 
-				print_grid(grid, 1);
+				if(f_slow == 1)
+				{
+					fflush(stdout);
+					if(f_very == 1)
+						usleep(50000);
+
+					count++;
+					print_grid(grid, 1);
+				}
+				
 				placable = check_all(grid, i, j);
 				
+				if(f_debug == 1)
+				{
+					printf("---------- DEBUG -----------\n\n");
+					printf("Current place (x:%d, y:%d) = %d\n", i, j, grid[i][j]);
+					printf("Current is right = %d\n", placable);
+					printf("Iteration counter = %d\n", count);
+					printf("\n---------- DEBUG -----------\n\n");
+				}
 			}
 
 			if (grid[i][j] == 10)
-				backtrack(grid, old, &i, &j);
-
+			{
+				if(i == 0 && j == 0)
+				{
+					stop = 1;
+					print_no_solve();
+				}
+				else
+					backtrack(grid, old, &i, &j);
+			}
 			else if(placable == 1)
 				advance(&i, &j);
 		}
 		else
-		{
 			advance(&i, &j);
-		}
 	}
 }
 
@@ -228,14 +281,19 @@ void print_grid(int target[9][9], int is_loading)
 	int i = 0;
 	int j = 0;
 
+	/* clear stdout */
 	printf("\033[H\033[J\n");
+
+	if(is_loading == 1)
+		print_loading();
+	
 	while(i < 9)
 	{
 		j = 0;
 
 		/* 3 lines gap */
 		if (i % 3 == 0 && i != 0)
-			printf("------------------------------\n");
+			printf("-------- + --------- + --------\n");
 
 		while(j < 9)
 		{
@@ -243,11 +301,11 @@ void print_grid(int target[9][9], int is_loading)
 			if (j % 3 == 0 && j != 0)
 				printf("|  ");
 
-			if (target[i][j] != 0)
+			/* print values but spaces when its 10 and 0 */
+			if (target[i][j] != 0 && target[i][j] != 10)
 				printf("%d  ", target[i][j]);
 			else
-				printf(".  ");
-			
+				printf("   ");
 			j++;
 		}
 		printf("\n");
@@ -255,11 +313,36 @@ void print_grid(int target[9][9], int is_loading)
 	}
 	printf("\n");
 
-	if(is_loading == 1)
-		printf("Tring to solve...\n\n");
+	if(is_loading == 0)
+	{
+		printf("This is the solution.\n\n");
+		printf("Use the flag [-h] to see all options.\n\n");
+	}
 }
 
-int get_arg(int target[9][9], int ac, char **av)
+void print_loading()
+{
+	char temp;
+
+	switch(count % 4)
+	{
+		case 0:
+			temp = '|';
+		break;
+		case 1:
+			temp = '/';
+		break;
+		case 2:
+			temp = '-';
+		break;
+		case 3:
+			temp = 92;
+		break;
+	}
+	printf("Loading %c\n\n", temp);
+}
+
+int get_out(int target[9][9], char **av)
 {
 	/* to return 0 if good everything good, else 1 */
 	int status = 0;
@@ -267,29 +350,141 @@ int get_arg(int target[9][9], int ac, char **av)
 	/* loops variables */
 	int i = 0;
 	int j = 0;
-	int cnt = 0;
 
-	/* temporary char */
-	char temp;
+	out = fopen(out_name, "w");
 
-	/* grid (file) */
-	FILE *fp;
-
-	if(ac < 2 || strcmp(av[1], "-h") == 0)
+	while(i < 9)
 	{
-		printf("\nUsage: %s [-h] input_file outpout_file\n\n", av[0]);
+		j = 0;
+		while(j < 9)
+		{
+			fprintf(out, "%d", target[i][j]);
+			j++;
+		}
+		fprintf(out, "\n");
+		i++;
+	}
+
+	fclose(out);
+
+	return status;
+}
+
+int get_arg(int target[9][9], int ac, char **av)
+{
+	/* to return 0 if good everything good, else 1 */
+	int status = 0;
+	int check_sol = 0;
+
+	/*temp file*/
+	int temp_arr[9][9];
+
+	/* loops variables */
+	int i = 0;
+	int j = 0;
+	int arg = 0;
+	int txt = 0;
+
+	/* temporary */
+	int f_fast = 0;
+	char temp;
+	
+	if(ac == 1)
+	{
+		print_usage(av[0]);
+		status = 1;
+	}
+	else if(ac == 2 && strcmp(av[1], "-h") != 0)
+	{
+		print_usage(av[0]);
+		status = 1;
+	}
+	else if(ac == 2 && strcmp(av[1], "-h") == 0)
+	{
+		printf("\nYou can run %s with some options.", av[0]);
+		printf("\nThose optionals arguments come after files argument.\n");
+		print_usage(av[0]);	
+		printf("FLAGS:");
+		printf("\n-slow or -fast:\n");
+		printf("\n	-slow = display the grid to see the resolution.");
+		printf("\n	-fast = dont display the grid, just output the file.");
+		printf("\n	-veryslow = refresh time is slower so you can read better (pretty nice with -debug).");
+		printf("\n	default = -slow.\n\n");
+		printf("\n-debug or -nodebug:\n");
+		printf("\n	-debug = display some debugs informations (-slow is needed risk of glitches).");
+		printf("\n	-nodebug = dont display debugs informations.");
+		printf("\n	default = -nodebug.\n\n");
+		printf("FILE:");
+		printf("\nformat:\n");
+		printf("\n	The file must contain 9 rows of 9 numbers between 0 and 9.");
+		printf("\n	No spaces, or other than numbers.");
+		printf("\n	The content is a sudoku grid, so if you put 0, its a blank square.");
+		printf("\n	If you put a number greater than 0, this number is not gonna change.");
+		printf("\n	Careful to use a valid grid, or the program will not use it.");
+		printf("\n\n	exemple of a valid file content to solve:\n\n");
+		printf("	003050800\n");
+		printf("	800900020\n");
+		printf("	926008045\n");
+		printf("	009400000\n");
+		printf("	730005100\n");
+		printf("	680001079\n");
+		printf("	068024000\n");
+		printf("	042000607\n");
+		printf("	009080054\n");
+		printf("\n\n");
 		status = 1;
 	}
 	else
 	{
 		fp = fopen(av[1], "r");
+		out = fopen(av[2], "r");
 
-		if (fp == NULL)
- 		{
-			printf("Cannot open the file  %s\n", av[1]);
+		if(fp == NULL)
+		{
+			printf("\nCannot find '%s' as input (sudoku grid) file.\n", av[1]);
+			print_help(av[0]);
 			status = 1;
 		}
-		else
+
+		/* get name of the output file */
+		out_name = av[2];
+	
+		/* check if the file have a solvable grid */
+		if(out == NULL)
+			check_sol = 1;
+
+		/* go though flags */
+		if (ac > 3)
+		{
+			arg = 3;
+			/* check flags and change settings */
+			while(arg < ac)
+			{
+				if(strcmp(av[arg], "-fast") == 0)
+				{
+					f_slow = 0;
+				}
+				if(strcmp(av[arg], "-slow") == 0)
+				{
+					f_slow = 1;
+				}
+				if(strcmp(av[arg], "-debug") == 0 && f_slow == 1)
+				{
+					f_debug = 1;
+				}
+				if(strcmp(av[arg], "-nodebug") == 0)
+				{
+					f_debug = 0;
+				}
+				if(strcmp(av[arg], "-veryslow") == 0)
+				{
+					f_very = 1;
+				}
+				arg++;
+			}
+		}
+
+		if(status == 0)
 		{
 			while(i < 9)
 			{
@@ -300,7 +495,6 @@ int get_arg(int target[9][9], int ac, char **av)
 					if(temp != '\n' && temp != '\r')
 					{
 						target[i][j] = uf_ctoi(temp);
-						cnt++;
 						j++;
 					}
 				}
@@ -309,5 +503,147 @@ int get_arg(int target[9][9], int ac, char **av)
 			fclose(fp);
 		}
 	}
+
+	/* Check if the grid has a solution */
+	if (check_sol == 1 && status == 0)
+	{
+		status = check_grid(target);
+		if(status == 1)
+			print_no_solve();
+	}
+	else if(check_sol == 0 && status == 0)
+	{
+		/* We are there because the output file already exist so we are gonna check if 
+		* it contain a solved grid of the input file */
+		i = 0;
+		while(i < 9)
+		{
+			j = 0;
+			while(j < 9)
+			{
+				temp = fgetc(out);
+				if(temp != '\n' && temp != '\r')
+				{
+					temp_arr[i][j] = uf_ctoi(temp);
+					j++;
+				}
+			}
+			i++;
+		}
+		/* return 1 if the  */
+		status = check_in_out(target, temp_arr);
+		if(status == 1)
+			printf("\nYou already have a solved version of '%s' in '%s'.\n\n", av[1], av[2]);
+	}
+
 	return status;
+}
+
+/* this funtion check if the out array is a solved version of the in array */
+int check_in_out(int in[9][9], int out[9][9])
+{
+	/* variables to return */
+	int to_return = 1;
+
+	/* loops variables */
+	int i = 0;
+	int j = 0;
+
+	/* first we check if the out array have the same start grid as in array */
+	while(i < 9)
+	{
+		j = 0;
+		while(j < 9)
+		{
+			if(in[i][j] != 0)
+			{
+				if(in[i][j] != out[i][j])
+					to_return = 0;
+			}
+
+			if(out[i][j] == 0)
+				to_return = 0;
+			j++;
+		}
+		i++;
+	}
+
+	return to_return;
+}
+
+int check_grid(int target[9][9])
+{
+	/* return 0 if the grid is good else 1 */
+	int to_return = 0;
+
+	/* loops variables */
+	int i = 0;
+	int j = 0;
+
+	/* Check if the grid have an error */
+	while(i < 9)
+	{
+		j = 0;
+		while(j < 9)
+		{
+			if(target[i][j] != 0)
+			{
+				if(check_all(target, i, j) == 0)
+					to_return = 1;
+			}
+			j++;
+		}
+		i++;
+	}
+
+	return to_return;	
+}
+
+void print_help(char *str)
+{
+	printf("If you need help you can type %s -h\n\n", str);
+}
+
+void print_no_solve()
+{
+	printf("\n\nThis grid cant be solved.\n\n");
+}
+
+void print_usage(char *str)
+{
+	printf("\nUsage: %s [-h see possible flags] <input_file> <outpout_file> [flags]\n\n", str);
+}
+
+int uf_atoi(char *str)
+{
+	int to_return = 0;
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		to_return = to_return * 10 + str[i] - '0';
+		i++;
+	}
+
+	return to_return;
+}
+
+int uf_ctoi(char c)
+{
+	int to_return;
+
+	to_return = c - '0';
+
+	return to_return;
+}
+
+void uf_split_atoi(int *target, char *str, int len)
+{
+	int i = 0;
+
+	while (i < len)
+	{
+		target[i] = str[i] - '0';
+		i++;
+	}
 }
--- sudokusolver_06-10-2020/useful.h
+++
@@ -1,8 +0,0 @@
-#ifndef USEFUL_LIB
-#define USEFUL_LIB
-
-int uf_atoi(char *str);
-int uf_ctoi(char c);
-void uf_split_atoi(int *target, char *str, int len);
-
-#endif
