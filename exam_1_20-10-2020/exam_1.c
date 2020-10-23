a81afd934d24129ca7f50b2fe45f6931ed053dde
l0th3r <alfabetatango9@gmail.com>
2020-10-23 15:05:49 +0200

    exam -> 10th exerice done

--- exam_1_20-10-2020/exam_1.c
+++ exam_1_20-10-2020/exam_1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <limits.h>
 
 /* EX 01 */
@@ -192,43 +193,184 @@ int dynamic_int_array_remove(dynamic_int_array* darray, size_t index)
 }
 
 /* EX 08 */
+
 dynamic_int_array* sub_dynamic_int_array(const dynamic_int_array* darray, size_t start, size_t end)
 {
-	int i = start;
-	dynamic_int_array *cpy = create_dynamic_int_array(end-start + 1);
+	int i = start; 
+	int j = 0;
+	dynamic_int_array *cpy;
 
-	while(i < end)
+	if(end > darray->size)
+		end = darray->size + 1;
+
+	cpy = create_dynamic_int_array(end - start - 1);
+
+	while(j < cpy->size || i < end)
 	{
-		cpy->array[i] = darray->array[i];
+		cpy->array[j] = darray->array[i];
+		j++;
 		i++;
 	}
 
 	return cpy;
 }
 
+dynamic_int_array* copy_dynamic_int_array(const dynamic_int_array* darray)
+{
+	dynamic_int_array *cpy = sub_dynamic_int_array(darray, 0, darray->size + 1);
+	return cpy;
+}
+
+/* EX 09 */
+
+char* read_file(const char* path)
+{
+	FILE * fp;
+	char *to_return;
+	char temp;
+	size_t len = 1;
+	int i = 0;
+
+	to_return = malloc(len * sizeof(char));
+
+	fp = fopen(path, "r");
+
+	if(fp != NULL)
+	{
+		do
+		{
+			to_return = realloc(to_return, len++ * sizeof(char));
+			temp = fgetc(fp);
+			to_return[i] = temp;
+			i++;
+		}
+		while(temp != EOF);
+		to_return = realloc(to_return, len++ * sizeof(char));
+		to_return[i] = '\0';
+	}
+
+	return to_return;
+}
+
+/* EX 10 */
+typedef struct {int number; char * file_path; char * text; int * link;} interactive_story_paragraph;
+
+interactive_story_paragraph* create_interactive_story(char* folder_path, int paragraph_count)
+{
+	interactive_story_paragraph* to_return = malloc(paragraph_count * sizeof(interactive_story_paragraph));
+
+	/* loops variables */
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int l = 0;
+	int m = 0;
+
+	/* declare temporary pointers */
+	char *ctemp;
+	int *ltemp;
+
+	while(i < paragraph_count)
+	{
+		to_return[i].number = i;
+
+		/* allocate */
+		ctemp = malloc(sizeof(char));
+		ltemp = malloc(sizeof(int));
+
+		to_return[i].link = malloc(sizeof(int));
+		to_return[i].file_path = malloc( (strlen(folder_path) + 20) * sizeof(char));
+
+		/* convert number to str */
+		sprintf(to_return[i].file_path, "%s/%d%c",folder_path, i, '\0');
+
+		to_return[i].text = read_file(to_return[i].file_path);
+
+		j = 0;
+		l = 0;
+		while(to_return[i].text[j] != '\0')
+		{
+			if(to_return[i].text[j] == '*')
+			{
+				k = 0;
+				j++;
+				while(to_return[i].text[j] != '*')
+				{
+					ctemp = realloc(ctemp, (k + 1) * sizeof(char));
+					ctemp[k] = to_return[i].text[j];
+					k++;
+					j++;
+				}
+				ctemp = realloc(ctemp, (k + 1) * sizeof(char));
+				ctemp[k] = '\0';
+
+				ltemp = realloc(ltemp, (l + 1) * sizeof(int));
+				ltemp[l] = atoi(ctemp);
+
+				l++;
+				j++;
+			}
+			else
+				j++;
+		}
+
+		ltemp = realloc(ltemp, (l + 1) * sizeof(int));
+		ltemp[l] = 0;
+		l++;
+
+		m = 0;
+		while(ltemp[m] != 0)
+		{
+			to_return[i].link = realloc(to_return[i].link, (m + 1) * sizeof(int));
+			to_return[i].link[m] = ltemp[m];
+			m++;
+		}
+
+		to_return[i].link = realloc(to_return[i].link, (m + 1) * sizeof(int));
+		to_return[i].link[m] = 0;
+
+		free(ctemp);
+		free(ltemp);
+		i++;
+	}
+
+	return to_return;
+}
+
+
+
+
+
+
 
 /* REMOVE */
 int main(void)
 {
 	int i = 0;
-	dynamic_int_array *pt = create_dynamic_int_array(10);
-	dynamic_int_array *cp;
-	dynamic_int_array_set(pt, 15, 20);
+	int j = 0;
 
-	cp = sub_dynamic_int_array(pt, 10, pt->size);
+	interactive_story_paragraph* ip = create_interactive_story("./toile", 46);
 
-
-	while(i < cp->size)
+	while(i < 2)
 	{
-		printf("%d, ", cp->array[i]);
+		printf("\n{\n\n");
+
+		printf("number = %d\n", ip[i].number);
+		printf("file path = %s\n", ip[i].file_path);
+		printf("\n%s\n", ip[i].text);
+		printf("link = ");
+		j = 0;
+		while(ip[i].link[j] != 0)
+		{
+			printf("%d, ", ip[i].link[j]);
+			j++;		
+		}
+
+		printf("\n\n}\n");
+		
 		i++;
 	}
-
-
-	printf("\nres = %d\n", dynamic_int_array_remove(pt, 14));
-
-	destroy_dynamic_int_array(pt);
-
+	
 	return 0;
 }
 /* REMOVE */
